1ca0fa4eb86566547df259c1237ea89d4edae73c
David Hakobyan <hackopiand@gmail.com>
2018-08-13 20:19:39 -0500

    2nd Commit
    Change-Id: I2664ff179939c805b4f507fc5de568d6732efeaf
    Signed-off-by: David Hakobyan <hackopiand@gmail.com>

---
+++ DemoGitHub/src/DemoTest.java
@@ -0,0 +1,17 @@
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("First commit 1st");
+		
+		/*
+		 * 
+		 * 
+		 * your code goes here
+		 * 
+		 * 	
+		 */
+	}
+
+}
